4c64dff9800c19c392b26cc88410142ea2b4cf36
michaelfestger <festger@gmail.com>
2017-04-13 16:33:25 -0500

    Finished the planets - array exercise.

---
+++ js/objects.js
@@ -0,0 +1,54 @@
+"use strict";
+// EXAMPLE 1
+var person = {};
+console.log(typeof person);
+person.firstName = "Mike";
+person.lastName = "Festger";
+console.log("Hello from " + person.firstName + " " + person.lastName + "!");
+console.log("----------------------");
+// EXAMPLE 2
+var shoppers = [
+    {name: 'Cameron', amount: 180},
+    {name: 'Ryan', amount: 250},
+    {name: 'George', amount: 320}
+];
+shoppers.forEach(function (shopper) {
+    if (shopper.amount >= 200) {
+        console.log("Name: " + shopper.name + ".");
+        console.log("Amount paid before discount: $" + shopper.amount);
+        console.log("Amount of discount (if applicable): 35%.");
+        var discountedAmount = shopper.amount * .35;
+        console.log("Amount after discount applied: $" + discountedAmount + ".");
+        console.log("--------------------");
+    } else {
+        console.log(shopper.name + " did not spend more than $200 so there was no discount.");
+        console.log("--------------------")
+    }
+});
+// EXAMPLE 3
+
+var books = [
+    {title: "The World According to Garp", author: {firstName: "John", lastName: "Irving"}},
+    {title: "Cat in the Hat", author: {firstName: "Dr.", lastName: "Suess"}},
+    {title: "Hamlet", author: {firstName: "William", lastName: "Shakespeare"}},
+    {title: "Eishnorn", author: {firstName: "Dan", lastName: "Abnett"}},
+    {title: "The Last Chancers", author: {firstName: "Gav", lastName: "Thorpe"}},
+    {title: "Fahrenheit 451", author: {firstName: "Ray", lastName: "Bradbury"}},
+    {title: "The DaVinci Code", author: {firstName: "Dan", lastName: "Brown"}},
+    {title: "To Kill a Mockingbird", author: {firstName: "Harper", lastName: "Lee"}},
+    {title: "The Dark Knight Returns", author: {firstName: "Frank", lastName: "Miller"}},
+    {title: "Brave New World", author: {firstName: "Aldous", lastName: "Huxley"}}
+];
+
+var i = 1;
+books.forEach(function (book) {
+    console.log("Book #" + i);
+    console.log("Title: " + book.title);
+    console.log("Author: " + book.author.firstName + " " + book.author.lastName);
+    console.log("---");
+    i++;
+});
+
+
+
+
